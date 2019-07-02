684a0c0ccfe917b0477dccde93ea49179961a2e9
Mitul Patel <39785177+bittricky@users.noreply.github.com>
2019-07-02 17:51:51 -0400

    details component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Search from './components/Search';
+import Details from './components/Details';
 
 import './App.css';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Search />
+      <Details title={'track title'} />
     </div>
   );
 }
